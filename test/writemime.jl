e407e18e0b00ae1dd080ffa01801a9295a4574ef
Ralph A. Smith <smith.ralph.a@gmail.com>
2018-08-20 22:30:40 -0400

    make testing independent of Images

--- test/writemime.jl
+++ test/writemime.jl
@@ -1,6 +1,17 @@
 using ImageShow, Colors, FixedPointNumbers, FileIO
-import Images: colorview, normedview, restrict
+import ImageCore: colorview, normedview
+# We jump through some hoops so that this test script may work
+# whether or not ImageTransformations (a fortiori Images) is loaded.
+# See below for details.
+
+# don't import ImageTransformations: restrict
+
 using Test
+
+workdir = joinpath(tempdir(), "Images")
+if !isdir(workdir)
+    mkdir(workdir)
+end
 
 @testset "show (MIME)" begin
     # Test that we remembered to turn off Colors.jl's colorswatch display
@@ -8,10 +19,6 @@ using Test
     @test !showable(MIME("image/svg+xml"), rand(RGB{N0f8},  5, 5))
     @test showable(MIME("image/png"), rand(Gray{N0f8}, 5, 5))
     @test showable(MIME("image/png"), rand(RGB{N0f8},  5, 5))
-    workdir = joinpath(tempdir(), "Images")
-    if !isdir(workdir)
-        mkdir(workdir)
-    end
     @testset "no compression or expansion" begin
         A = N0f8[0.01 0.99; 0.25 0.75]
         fn = joinpath(workdir, "writemime.png")
@@ -86,12 +93,38 @@ using Test
         end
         @test load(fn) == A[[1,1,2,2],[1,1,2,2]]
     end
+end
+
+# We do this after the FileIO backend has gotten comfortable.
+const have_restrict = Ref{Bool}(false)
+const restrict_mod = Ref{Module}()
+for mod in values(Base.loaded_modules)
+    if string(mod) == "ImageTransformations"
+        have_restrict[] = true
+        restrict_mod[] = mod
+    end
+end
+
+if have_restrict[]
+    @info "Tests will use restrict from ImageTransformations"
+    const restrict = restrict_mod[].restrict
+end
+
+@testset "Big images and matrices" begin
     @testset "big images (use of restrict)" begin
         A = N0f8[0.01 0.4 0.99; 0.25 0.8 0.75; 0.6 0.2 0.0]
-        Ar = restrict(A)
+        if have_restrict[]
+            Ar = restrict(A)
+        else
+            Ar = N0f8[0.01 0.99; 0.6 0.0]
+        end
         fn = joinpath(workdir, "writemime.png")
         open(fn, "w") do file
-            show(file, MIME("image/png"), Gray.(A), minpixels=0, maxpixels=5)
+            if !have_restrict[]
+                @test_logs (:info, r"^For better quality") show(file, MIME("image/png"), Gray.(A), minpixels=0, maxpixels=5)
+            else
+                show(file, MIME("image/png"), Gray.(A), minpixels=0, maxpixels=5)
+            end
         end
         @test load(fn) == N0f8.(Ar)
         # a genuinely big image (tests the defaults)
@@ -101,7 +134,12 @@ using Test
             show(file, MIME("image/png"), abig, maxpixels=10^6)
         end
         b = load(fn)
-        @test b == N0f8.(restrict(abig, (1,2)))
+        if have_restrict[]
+            btmp = restrict(abig, (1,2))
+        else
+            btmp = abig[1:2:end,1:2:end]
+        end
+        @test b == N0f8.(btmp)
     end
     @testset "display matrix of images" begin
         img() = colorview(Gray, rand([0.250 0.5; 0.75 1.0], rand(2:10), rand(2:10)))
@@ -119,7 +157,13 @@ using Test
         @test !applicable(ImageShow._show_odd, io, MIME"text/html"(), flat_imgs)
         @test !applicable(ImageShow._show_even, io, MIME"text/html"(), flat_imgs)
     end
-    rm(workdir, recursive=true)
 end
+try
+    # if this fails, it is not our fault
+    rm(workdir, recursive=true)
+catch
+    # do nothing
+end
+
 
 nothing
