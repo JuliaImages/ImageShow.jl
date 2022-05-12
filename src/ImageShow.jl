e75afdd112e1ce692f2334b60b99125a5d684d2d
Johnny Chen <johnnychen94@hotmail.com>
2022-05-12 18:11:29 +0800

    opt-in HTML render (#51)

--- src/ImageShow.jl
+++ src/ImageShow.jl
@@ -9,6 +9,13 @@ using StackViews
 using ImageCore.MappedArrays
 using ImageCore.PaddedViews
 
+# opt-in HTML render for image
+# work around https://github.com/JuliaImages/ImageShow.jl/pull/50#issuecomment-1124132500
+const _ENABLE_HTML = Ref(false)
+enable_html_render() = _ENABLE_HTML[] = true
+disable_html_render() = _ENABLE_HTML[] = false
+Base.showable(::MIME"text/html", ::AbstractMatrix{<:Colorant}) = _ENABLE_HTML[]
+
 include("showmime.jl")
 include("gif.jl")
 include("multipage.jl")
