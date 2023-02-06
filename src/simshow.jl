8d67c2eec69435d89f8ac9fb5a98fc9c1a0cf2dd
roflmaostc <fxw+git@mailbox.org>
2023-02-06 09:22:29 +0100

    Remove function f and rename abs\gamma to \gamma in both cases

--- src/simshow.jl
+++ src/simshow.jl
@@ -1,8 +1,7 @@
 export simshow
 
 """
-    simshow(arr; set_one=false, set_zero=false,
-                f=nothing, γ=1)
+    simshow(arr; set_one=false, set_zero=false, γ=1)
 
 Displays a real valued array . Brightness encodes magnitude.
 Works within Jupyter and Pluto.
@@ -10,16 +9,15 @@ Works within Jupyter and Pluto.
 The transforms are applied in that order.
 * `set_zero=false` subtracts the minimum to set minimum to 1
 * `set_one=false` divides by the maximum to set maximum to 1
-* `f` applies an arbitrary function to the abs array
 * `γ` applies a gamma correction to the abs 
 * `cmap=:gray` applies a colormap provided by ColorSchemes.jl. If `cmap=:gray` simply `Colors.Gray` is used
     and with different colormaps the result is an `Colors.RGB` element type
 """
 function simshow(arr::AbstractArray{T};
                  set_one=true, set_zero=false,
-                 f = nothing,
                  γ = one(T),
                  cmap=:gray) where {T<:Real}
+
     arr = set_zero ? arr .- minimum(arr) : arr
 
     if set_one
@@ -28,8 +26,6 @@ function simshow(arr::AbstractArray{T};
             arr = arr ./ maximum(arr)
         end
     end
-
-    arr = isnothing(f) ? arr : f(arr)
 
     if !isone(γ)
         arr = arr .^ γ
@@ -48,32 +44,22 @@ end
 
 Displays a complex array. Color encodes phase, brightness encodes magnitude.
 Works within Jupyter and Pluto.
+
 # Keyword args
+
 The transforms are applied in that order.
-* `f` applies a function `f` to the array.
-* `absf` applies a function `absf` to the absolute of the array
-* `absγ` applies a gamma correction to the abs 
+* `γ` applies a gamma correction to the abs 
 """
 function simshow(arr::AbstractArray{T};
-                 f=nothing,
-                 absγ=one(T),
-                 absf=nothing) where (T<:Complex)
-
-    if !isnothing(f)
-        arr = f(arr)
-    end
+                 γ=one(T)) where (T<:Complex)
 
     Tr = real(T)
     # scale abs to 1
     absarr = abs.(arr)
     absarr ./= maximum(absarr)
 
-    if !isnothing(absf)
-        absarr .= absf(absarr)
-    end
-    
-    if !isone(absγ)
-        absarr .= absarr .^ absγ
+    if !isone(γ)
+        absarr .= absarr .^ γ
     end
 
     angarr = angle.(arr) ./ Tr(2pi) * Tr(360)
@@ -85,6 +71,7 @@ end
 
 """
     simshow(arr::AbstractArray{<:Colors.ColorTypes.Colorant})
+
 If `simshow` receives an array which already contains color information, just display it.
 """
 function simshow(arr::AbstractArray{<:Colors.ColorTypes.Colorant})
--- test/simshow.jl
+++ test/simshow.jl
@@ -9,7 +9,6 @@
     @test simshow([0.1, -0.1], set_one = false, set_zero = true) == ColorTypes.Gray{Float64}[Gray{Float64}(0.2), Gray{Float64}(0.0)]
     @test simshow([0.1, -0.1], set_one = false, set_zero = false) == ColorTypes.Gray{Float64}[Gray{Float64}(0.1), Gray{Float64}(-0.1)]
     @test simshow([0.1, 0], γ = 2, set_one = false) == ColorTypes.Gray{Float64}[Gray{Float64}(0.010000000000000002), Gray{Float64}(0.0)]
-    @test simshow([0.1, 0], f = (x-> zeros((2,))), set_one = false) == ColorTypes.Gray{Float64}[Gray{Float64}(0.0), Gray{Float64}(0.0)]
 
 
     @test simshow([0, 1, 2], cmap = :thermal) == ColorTypes.RGB{Float64}[RGB{Float64}(0.015556013331540799,0.13824424546464084,0.2018108864558305), RGB{Float64}(0.6893346807608062,0.37270416310862364,0.5096912535037159), RGB{Float64}(0.9090418416674036,0.9821574063216706,0.3555078064299531)]
@@ -20,17 +19,14 @@ end
 @testset "Complex Arrays" begin
     @test simshow([1.0, 1im, -1, -1im, 1.0 - 0.0001im]) == ColorTypes.HSV{Float64}[HSV{Float64}(0.0,1.0,0.999999995), HSV{Float64}(90.0,1.0,0.999999995), HSV{Float64}(180.0,1.0,0.999999995), HSV{Float64}(-90.0,1.0,0.999999995), HSV{Float64}(-0.00572957793220964,1.0,1.0)]
 
-    @test simshow([1.0 / 2, (1im) / 2, -1 / 2, (-1im) / 2, 1.0 - 0.0001im], absγ = 2) == ColorTypes.HSV{Float64}[HSV{Float64}(0.0,1.0,0.24999999750000002), HSV{Float64}(90.0,1.0,0.24999999750000002), HSV{Float64}(180.0,1.0,0.24999999750000002), HSV{Float64}(-90.0,1.0,0.24999999750000002), HSV{Float64}(-0.00572957793220964,1.0,1.0)]
-
-
-    @test simshow([1.0, (1im) / 4, -1 / 8, (-1im) / 2, 1.0 - 0.0001im], absf = (x->begin
+    @test simshow([1.0 / 2, (1im) / 2, -1 / 2, (-1im) / 2, 1.0 - 0.0001im], γ = 2) == ColorTypes.HSV{Float64}[HSV{Float64}(0.0,1.0,0.24999999750000002), HSV{Float64}(90.0,1.0,0.24999999750000002), HSV{Float64}(180.0,1.0,0.24999999750000002), HSV{Float64}(-90.0,1.0,0.24999999750000002), HSV{Float64}(-0.00572957793220964,1.0,1.0)]
+    absf = (x->begin
                 y = copy(x)
                 y .= x[1]
                 y
-            end)) == ColorTypes.HSV{Float64}[HSV{Float64}(0.0,1.0,0.999999995), HSV{Float64}(90.0,1.0,0.999999995), HSV{Float64}(180.0,1.0,0.999999995), HSV{Float64}(-90.0,1.0,0.999999995), HSV{Float64}(-0.00572957793220964,1.0,0.999999995)]
-    simshow([0.1, 0im], f = (x->begin
-                1.1 .* ones(ComplexF32, (2,))
-            end)) == HSV{Float64}[HSV{Float64}(0.0,1.0,1.0), HSV{Float64}(0.0,1.0,1.0)]
+            end)
+
+    @test simshow([1.1 + 0im, 1.1]) == HSV{Float64}[HSV{Float64}(0.0,1.0,1.0), HSV{Float64}(0.0,1.0,1.0)]
 end
 
 @testset "Colorant Array" begin
