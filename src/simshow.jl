36ad044257235be2eb7f0763a95ceafee8babf1e
roflmaostc <fxw+git@mailbox.org>
2023-02-13 17:02:41 +0100

    Fix some docs [skip ci]

--- src/simshow.jl
+++ src/simshow.jl
@@ -13,8 +13,8 @@ The transforms are applied in that order.
 * `set_one=true` divides by the maximum to set maximum to 1
 * `γ` applies a gamma correction
 * `cmap=:gray` applies a colormap provided by ColorSchemes.jl. If `cmap=:gray` simply `Colors.Gray` is used
-    and with different colormaps the result is an `Colors.RGB` element type. To use the different colormaps
-    install ColorSchemes.jl and try colormaps such as `:jet`, `:deep`, `thermal`, etc.
+    and with different colormaps the result is an `Colors.RGB` element type.  
+    You can try `:jet`, `:deep`, `thermal` or different ones by reading the catalogue of ColorSchemes.jl
 """
 function simshow(arr::AbstractArray{T};
                  set_zero=false, set_one=true,
